8cef1ac6b8eae03d01d39120624f02c7c5f836f4
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2021-03-30 13:18:16 +0200

    add P,R,F1 metrics, and support saved models 🔥

--- run_exBERT_triple_classifier.py
+++ run_exBERT_triple_classifier.py
@@ -1,174 +1,27 @@
-from typing import Tuple, List
 import logging
 import argparse
 import torch
-import csv
-import sys
 import os
-import random
-from transformers import BertTokenizerFast
 from transformers import BertForSequenceClassification, Trainer, TrainingArguments
-
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from processors import KGProcessor
 
 logger = logging.getLogger(__name__)
 
 
-class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels)
-
-
-class KGProcessor:
-
-    def __init__(self, tokenizer: str):
-        self.labels = set()
-        self.tokenizer = BertTokenizerFast.from_pretrained(tokenizer)
-
-    @classmethod
-    def _read_tsv(cls, input_file, quotechar=None):
-        """Reads a tab separated value file."""
-        with open(input_file, "r", encoding="utf-8") as f:
-            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
-            lines = []
-            for line in reader:
-                if sys.version_info[0] == 2:
-                    line = list(cell.unicode('utf-8') for cell in line)
-                lines.append(line)
-            return lines
-
-    def get_relations(self, data_dir):
-        """Gets all labels (relations) in the knowledge graph."""
-        with open(os.path.join(data_dir, "relations.txt"), 'r') as f:
-            lines = f.readlines()
-            relations = []
-            for line in lines:
-                relations.append(line.strip())
-        return relations
-
-    def get_labels(self, data_dir):
-        """Gets all labels (0, 1) for triples in the knowledge graph."""
-        return ["0", "1"]
-
-    def get_entities(self, data_dir):
-        """Gets all entities in the knowledge graph."""
-        # return list(self.labels)
-        with open(os.path.join(data_dir, "entities.txt"), 'r') as f:
-            lines = f.readlines()
-            entities = []
-            for line in lines:
-                entities.append(line.strip())
-        return entities
-
-    def get_train_triples(self, data_dir):
-        """Gets training triples."""
-        return self._read_tsv(os.path.join(data_dir, "train.tsv"))
-
-    def get_dev_triples(self, data_dir):
-        """Gets validation triples."""
-        return self._read_tsv(os.path.join(data_dir, "dev.tsv"))
-
-    def get_test_triples(self, data_dir):
-        """Gets test triples."""
-        return self._read_tsv(os.path.join(data_dir, "test.tsv"))
-
-    @staticmethod
-    def corrupt_head_tail(ent2text, entities, line, lines_str_set, text_a, text_b, text_c):
-        rnd = random.random()
-        texts = []
-        labels = []
-        if rnd <= 0.5:
-            # corrupting head
-            tmp_head = ''
-            while True:
-                tmp_ent_list = set(entities)
-                tmp_ent_list.remove(line[0])
-                tmp_ent_list = list(tmp_ent_list)
-                tmp_head = random.choice(tmp_ent_list)
-                tmp_triple_str = tmp_head + '\t' + line[1] + '\t' + line[2]
-                if tmp_triple_str not in lines_str_set:
-                    break
-            tmp_head_text = ent2text[tmp_head]
-            texts.append(f"[CLS] {tmp_head_text} [SEP] {text_b} [SEP] {text_c} [SEP]")
-            labels.append(0)
-        else:
-            # corrupting tail
-            tmp_tail = ''
-            while True:
-                tmp_ent_list = set(entities)
-                tmp_ent_list.remove(line[2])
-                tmp_ent_list = list(tmp_ent_list)
-                tmp_tail = random.choice(tmp_ent_list)
-                tmp_triple_str = line[0] + '\t' + line[1] + '\t' + tmp_tail
-                if tmp_triple_str not in lines_str_set:
-                    break
-            tmp_tail_text = ent2text[tmp_tail]
-            texts.append(f"[CLS] {text_a} [SEP] {text_b} [SEP] {tmp_tail_text} [SEP]")
-            labels.append(0)
-        return texts, labels
-
-    def create_datasets(self, data_dir: str) -> Tuple[CustomDataset, CustomDataset, CustomDataset]:
-        train_lines = self._read_tsv(os.path.join(data_dir, "train.tsv"))
-        dev_lines = self._read_tsv(os.path.join(data_dir, "dev.tsv"))
-        test_lines = self._read_tsv(os.path.join(data_dir, "test.tsv"))
-
-        train_dataset = self.transform_portion_to_dataset(data_dir, train_lines)
-        dev_dataset = self.transform_portion_to_dataset(data_dir, dev_lines)
-        test_dataset = self.transform_portion_to_dataset(data_dir, test_lines)
-
-        return train_dataset, dev_dataset, test_dataset
-
-    def transform_portion_to_dataset(self, data_dir: str, lines: List) -> CustomDataset:
-        ent2text = {}
-        with open(os.path.join(data_dir, "entity2text.txt"), 'r', encoding='utf8') as f:
-            ent_lines = f.readlines()
-            for line in ent_lines:
-                temp = line.strip().split('\t')
-                if len(temp) == 2:
-                    ent2text[temp[0]] = temp[1]
-        entities = list(ent2text.keys())
-        rel2text = {}
-        with open(os.path.join(data_dir, "relation2text.txt"), 'r', encoding='utf8') as f:
-            rel_lines = f.readlines()
-            for line in rel_lines:
-                temp = line.strip().split('\t')
-                rel2text[temp[0]] = temp[1]
-        lines_str_set = set(['\t'.join(line) for line in lines])
-        texts = []
-        labels = []
-        for (i, line) in enumerate(lines):
-            with_labels = False
-            if len(line) > 3:
-                triple_label = line[3]
-                with_labels = True
-                if triple_label == "1":
-                    label = 1
-                else:
-                    label = 0
-            head_ent_text = ent2text[line[0]]
-            tail_ent_text = ent2text[line[2]]
-            relation_text = rel2text[line[1]]
-            texts.append(f"[CLS] {head_ent_text} [SEP] {relation_text} [SEP] {tail_ent_text} [SEP]")
-            if with_labels:
-                labels.append(label)
-            else:
-                labels.append(1)
-                corrupt_texts, corrupt_labels = self.corrupt_head_tail(ent2text, entities, line, lines_str_set,
-                                                                       head_ent_text, relation_text, tail_ent_text)
-                texts += corrupt_texts
-                labels += corrupt_labels
-        encodings = self.tokenizer(texts, truncation=True, padding=True)
-        ds = CustomDataset(encodings, labels)
-        # ds.set_format("torch", column=["input_ids"])
-        return ds
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    simple_accuracy = (preds == labels).mean()
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall,
+        'simple_accuracy': simple_accuracy
+    }
 
 
 def main():
@@ -187,7 +40,11 @@ def main():
                         type=str,
                         required=True,
                         help="The output directory where the model predictions and checkpoints will be written.")
-
+    # Pickup parameters
+    parser.add_argument("--custom_model",
+                        default=None,
+                        type=str,
+                        help="The path to a trained model (custom PyTorch model)")
     # Other parameters
     parser.add_argument("--cache_dir",
                         default="",
@@ -278,7 +135,8 @@ def main():
         raise ValueError("Invalid gradient_accumulation_steps parameter: {}, should be >= 1".format(
             args.gradient_accumulation_steps))
 
-    kg = KGProcessor(args.bert_model)
+    kg = KGProcessor(
+        args.custom_model if args.custom_model is not None and os.path.exists(args.custom_model) else args.bert_model)
 
     training_args = TrainingArguments(
         output_dir=args.output_dir,  # output directory
@@ -300,20 +158,25 @@ def main():
         no_cuda=args.no_cuda
     )
 
-    model = BertForSequenceClassification.from_pretrained(args.bert_model)
+    model = BertForSequenceClassification.from_pretrained(
+        args.custom_model if args.custom_model is not None and os.path.exists(args.custom_model) else args.bert_model)
     train_ds, eval_ds, test_ds = kg.create_datasets(args.data_dir)
 
     trainer = Trainer(
         model=model,  # the instantiated 🤗 Transformers model to be trained
         args=training_args,  # training arguments, defined above
         train_dataset=train_ds,  # training dataset
-        eval_dataset=eval_ds  # evaluation dataset
+        eval_dataset=eval_ds,  # evaluation dataset
+        compute_metrics=compute_metrics  # evaluation dataset
     )
 
-    trainer.train()
-    model.save_pretrained(args.output_dir)
-    results = trainer.predict(test_ds)
-    print(results.metrics)
+    if args.do_train or args.do_eval:
+        trainer.train()
+    if not (args.custom_model is not None and os.path.exists(args.custom_model)):
+        trainer.save_model(args.output_dir)
+    if args.do_predict:
+        results = trainer.predict(test_ds)
+        print(results.metrics)
 
 
 if __name__ == "__main__":
